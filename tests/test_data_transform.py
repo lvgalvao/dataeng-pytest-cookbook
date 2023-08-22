0666ffa548a025a3fe41c53ee8ee699b1fe32ae3
Luciano Filho <lvgalvaofilho@gmail.com>
2023-08-22 07:49:23 -0300

    update

--- tests/test_data_transform.py
+++ tests/test_data_transform.py
@@ -1,13 +1,11 @@
-from src.data_transform import clean_data_lowe
+from src.data_transform import clean_data_lower
 
 def test_data_transform_clean_data_lower():
     # Given
     entrada = "PYTHON"
     esperado = "python"
-
     # When
     resultado = clean_data_lowe(entrada)
-
     # Then
     assert resultado == esperado
 
@@ -15,9 +13,7 @@ def test_data_transform_clean_data_lower_com_espacos():
     # Given
     entrada = "   HELLO WORLD   "
     esperado = "hello world"
-
     # When
     resultado = clean_data_lowe(entrada)
-
     # Then
     assert resultado == esperado
