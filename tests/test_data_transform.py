fef6c24e4b0524390771de3a6cbd87a315b0197a
Luciano Filho <lvgalvaofilho@gmail.com>
2023-08-22 07:47:07 -0300

    tests

--- tests/test_data_transform.py
+++ tests/test_data_transform.py
@@ -1,12 +1,5 @@
 from src.data_transform import clean_data_lowe
 
-"""
-Todo teste é formado por 3 etapas
-1. Given: Onde você prepara o ambiente para o teste
-2. When: Onde você executa o código que você quer testar
-3. Then: Onde você verifica se o resultado é o esperado
-
-"""
 def test_data_transform_clean_data_lower():
     # Given
     entrada = "PYTHON"
